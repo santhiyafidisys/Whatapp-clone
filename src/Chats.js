461d2658858d214eae704bee8c4303f2189184d3
santhiya <santhiya2712@gmail.com>
2021-05-06 20:25:57 +0530

    changes in component

---
+++ src/Chats.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styles from './Whatapp.module.css';
+
+class Chatbox extends React.Component{
+    render()
+    {
+      return(
+			<p className={styles.chatsstyles}></p>
+			)
+    }
+}
+export default Chatbox;
