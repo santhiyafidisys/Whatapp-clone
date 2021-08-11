2dd4074d6e4fc7ae13c3fe8e713c48a7adbc16a8
santhiya <santhiya2712@gmail.com>
2021-08-11 19:15:05 +0530

    alignment

--- src/Leftcontainer.js
+++ src/Leftcontainer.js
@@ -3,30 +3,22 @@ import ReactDOM from 'react-dom';
 import styles from './Whatapp.module.css';
 import Userdetails from './Userdetails';
 import Avatar from './Avatar';
-
-
 class Leftmenu extends React.Component{
-    render()
-      {
-        return (
-					<div className={styles.menu}>
-						 <Avatar image="dps8.png"/>
-						<div className={styles.menuright}>
-						<i class="fa fa-envelope"></i>	
-							<div>
-							<div className={styles.leftside}></div>
+  render()
+    {
+      return (
+				<div className={styles.menu}>
+					<Avatar image="dps8.png"/>
+					<div className={styles.menuright}>
+					<i class="fa fa-envelope"></i>	
+					<div>
 						<div className={styles.leftside}></div>
 						<div className={styles.leftside}></div>
-
-							</div>
-						 
-							</div>
-
-						 </div>
-						
-						
-					
-				)
-          }
-      }
-      export default Leftmenu;
+						<div className={styles.leftside}></div>
+					</div>
+					</div>
+				</div>
+			)
+    }
+  }
+export default Leftmenu;
